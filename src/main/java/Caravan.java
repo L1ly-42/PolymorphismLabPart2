9b4be0102f021e8db650ce3547b15242d5de0f9a
Leila Peltier <l_peltier@outlook.com>
2024-02-13 14:46:03 +0000

    Created Caravan class minus the interface-related method

---
+++ src/main/java/Caravan.java
@@ -0,0 +1,41 @@
+public class Caravan {
+    private String dateOfManufacture;
+    private int numberOfWheels;
+    private double rentPerSeason;
+    private boolean needRepairs;
+
+    public Caravan(String dateOfManufacture, double rentPerSeason){
+        this.dateOfManufacture = dateOfManufacture;
+        this.rentPerSeason = rentPerSeason;
+        this.numberOfWheels = 2;
+        this.needRepairs = false;
+    }
+
+    public String getDateOfManufacture() {
+        return this.dateOfManufacture;
+    }
+
+    public int getNumberOfWheels() {
+        return this.numberOfWheels;
+    }
+
+    public void setNumberOfWheels(int wheels) {
+        this.numberOfWheels = wheels;
+    }
+
+    public double getRentPerSeason() {
+        return this.rentPerSeason;
+    }
+
+    public void setRentPerSeason(double rent) {
+        this.rentPerSeason = rent;
+    }
+
+    public boolean getNeedRepairs() {
+        return this.needRepairs;
+    }
+
+    public void setNeedRepairs(boolean needRepairs) {
+        this.needRepairs = needRepairs;
+    }
+}
