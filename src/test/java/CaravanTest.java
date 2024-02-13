ce9786744e2f15c6520c0927f623822cbf356a54
Leila Peltier <l_peltier@outlook.com>
2024-02-13 15:07:31 +0000

    added accompanying Caravan class tests

---
+++ src/test/java/CaravanTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CaravanTest {
+
+    private Caravan rv;
+
+    @BeforeEach
+    public void setUp(){
+        rv = new Caravan("21/11/2007",800);
+    }
+
+    @Test
+    public void canGetDateOfManufacture(){
+        assertThat(rv.getDateOfManufacture()).isEqualTo("21/11/2007");
+    }
+
+    @Test
+    public void canGetNumberOfWheels(){
+        assertThat(rv.getNumberOfWheels()).isEqualTo(2);
+    }
+
+    @Test
+    public void canSetNumberOfWheels(){
+        rv.setNumberOfWheels(4);
+        assertThat(rv.getNumberOfWheels()).isEqualTo(4);
+    }
+
+    @Test
+    public void canGetRentPerSeason(){
+        assertThat(rv.getRentPerSeason()).isEqualTo(800);
+    }
+
+    @Test
+    public void canSetRentPerSeason(){
+        rv.setRentPerSeason(10);
+        assertThat(rv.getRentPerSeason()).isEqualTo(10);
+    }
+
+    @Test
+    public void canGetNeedRepairs(){
+        assertThat(rv.getNeedRepairs()).isEqualTo(false);
+    }
+
+    @Test
+    public void canSetNeedRepairs(){
+        rv.setNeedRepairs(true);
+        assertThat(rv.getNeedRepairs()).isEqualTo(true);
+    }
+
+
+
+}
